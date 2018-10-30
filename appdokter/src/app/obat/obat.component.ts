f0243a352483858178a6a62bdf362e481d355110
ariDwi <yohanes.ari90@gmail.com>
2018-10-30 20:56:26 +0700

    angular client

---
+++ appdokter/src/app/obat/obat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-obat',
+  templateUrl: './obat.component.html',
+  styleUrls: ['./obat.component.css']
+})
+export class ObatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
