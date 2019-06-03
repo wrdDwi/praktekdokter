2672c57d715a4692a7ea1fe5161998659e9d975a
ari <yohanes.ari90@gmail.com>
2019-06-03 17:59:00 +0700

    update list obat

--- appdokter/src/app/layout/obat/new-obat/new-obat.component.ts
+++ appdokter/src/app/layout/obat/new-obat/new-obat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-new-obat',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewObatComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+  }
+  // private listJenisObat = JenisObat[] = [];
+  private kdObats: FormControl;
+  private namaObats: FormControl;
+  private jenisObats: FormControl;
+  private stock: FormControl;
+  private hargaBeli: FormControl;
+  private hargaJual: FormControl;
+  private kdSatuan: FormControl;
+  private formObats: FormGroup;
+  private errMsg: string[];
+  // tslint:disable-next-line:no-inferrable-types
+  private validEdit: boolean = false;
+
 
   ngOnInit() {
+    this.errMsg = [];
+
+    this.route.data.forEach(data => {
+
+    });
+  }
+  initFormObat() {
+    this.kdObats = new FormControl('', [Validators.required]);
+    this.namaObats = new FormControl('', [Validators.required]);
+    this.jenisObats = new FormControl('', [Validators.required]);
+    this.stock = new FormControl('', []);
+    this.hargaBeli = new FormControl('', []);
+    this.hargaJual = new FormControl('', []);
+    this.kdSatuan = new FormControl('', []);
+    this.formObats = new FormGroup({
+      kdObats: this.kdObats,
+      namaObats: this.namaObats,
+      jenisObats: this.jenisObats,
+      stock: this.stock,
+      hargaBeli: this.hargaBeli,
+      hargaJual: this.hargaJual,
+      kdSatuan: this.kdSatuan
+    });
   }
 
 }
